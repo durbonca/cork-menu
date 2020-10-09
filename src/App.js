58cfde6299d76da91185b96b7d3c7c9e1cbc73cf
Manuel Duran <durbonca@gmail.com>
2020-10-08 21:06:49 -0300

    correct data import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Family from './components/Family';
-import data from './data/data';
+
 
 function App() {
   
   return (
     <div className="App">
-        <Family products="data"/> 
+        <Family/> 
     </div>
   );
 }
