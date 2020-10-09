4ab2bf06d474d46b6e43f3c6622e31e2a9cb3f92
Manuel Duran <durbonca@gmail.com>
2020-10-09 08:54:28 -0300

    importing food data
    import all the food data on the src/data/data.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,8 +6,8 @@ class Family extends React.Component {
         return  <div>
                  {this.props.product.map(function (product){ 
                     return  <div className="row d-flex mt-3 item">
-                                <h4 className="col-8">{product.name}</h4>
-                                <h6 className="col-4 text-right">{product.price}</h6>
+                                <h4 className="col-8 text-left">{product.name}</h4>
+                                <h6 className="col-4 text-right">$ {product.price}</h6>
                                 <span className="col-12">{product.description}</span>
                             </div>
                     })} 
--- src/data/data.js
+++ src/data/data.js
@@ -1,33 +1,263 @@
 const dataProducts = [
-                { title: 'carnes',
+                { title: 'BRUNCH',
+                    products:[
+                                {
+                                    id : 1,    
+                                    name: 'Omelette	',
+                                    description: '(Maiz, jamón, queso, tostadas, vegetales y bebida caliente)',
+                                    price: '13.000'
+                                },
+                                {
+                                    id : 2,    
+                                    name: 'Tabliao',
+                                    description: '(Carne desmechada, frijol, arepa asada, huevos revueltos, queso rallado y jugo)',
+                                    price: '15.000'
+                                },
+                                {
+                                    id:3,
+                                    name:'Calentado Cork',
+                                    description:'(calentado de frijol, huevo al gusto, patacón, chorizo y bebida caliente)',
+                                    price:'13.900'
+                                },
+                                {
+                                    id:4,
+                                    name:'Waffle',
+                                    description:'(Waffle con frutas)',
+                                    price:'10000'
+                                }
+                    ]
+                    },
+                    { title: 'ENTRADAS',
+                    products:[
+                                {
+                                id : 1,    
+                                name: 'Alitas BBQ',
+                                description: '(Cinco alitas de pollo lacadas con salsa BBQ de maracuyá)',
+                                price: '16000'
+                                },
+                                {
+                                id : 2,    
+                                name: 'Alitas BBQ',
+                                description: '(Diez alitas de pollo lacadas con salsa BBQ de maracuyá)',
+                                price: '28000'
+                                },
+                                {
+                                id : 3,    
+                                name: 'Empanadas Cork',
+                                description: '(Seis empanadas cork medianas, acompañadas de ají y limón)',
+                                price: '16000'
+                                },
+                                {
+                                id : 4,    
+                                name: 'Canasta de Pollo Agridulce',
+                                description: '(Tres canastas de pollo en cuadros, vegetales asados y piña)',
+                                price: '18000'
+                                },
+                                {
+                                id : 5,    
+                                name: 'Salchicha Alemana',
+                                description: '(Ternera o Res. Cortada en trozos, acompañado de papas y salsas)',
+                                price: '17000'
+                                }
+                    ]
+                    },
+                    { title: 'WRAPS',
+                    products:[
+                                {
+                                id : 1,    
+                                name: ' Wraps Pollo',
+                                description: '',
+                                price: '19.000'
+                                },
+                                {
+                                id : 2,    
+                                name: ' Wraps Ropa vieja',
+                                description: '',
+                                price: '19.000'
+                                }
+                    ]
+                    },
+                    { title: 'PLATOS',
+                    products:[
+                                {
+                                id : 1,    
+                                name: 'Lomo a la parrilla',
+                                description: '(Res o pollo)',
+                                price: '27.000'
+                                },
+                                {
+                                id : 2,    
+                                name: 'Lomo en salsa de champiñones',
+                                description: '(Opción de Res o pollo)',
+                                price: '28.000'
+                                },
+                                {
+                                id : 3,    
+                                name: 'Lomo en salsa Mexicana',
+                                description: '(Opción de Res o pollo)',
+                                price: '28.000'
+                                },
+                                {
+                                id : 4,    
+                                name: 'Costillitas BBQ o a la Parrilla',
+                                description: '',
+                                price: '26.000'
+                                },
+                                {
+                                id : 5,    
+                                name: 'Nuggets de Pollo',
+                                description: '',
+                                price: '17.000'
+                                },
+                                {
+                                id : 6,    
+                                name: 'Brochetas de Pollo',
+                                description: '',
+                                price: '17.000'
+                                }
+                    ]
+                    },
+                    { title: 'HAMBURGUESAS',
+                    products:[
+                                {
+                                id : 1,    
+                                name: 'Hamburguesa Dublin',
+                                description: '(180 GR)',
+                                price: '18.000'
+                                },
+                                {
+                                id : 2,    
+                                name: 'Hamburguesa Doble Carne',
+                                description: '',
+                                price: '23.500'
+                                },
+                                {
+                                id : 3,    
+                                name: 'Hamburguesa Mexicana',
+                                description: '(Frijol, guacamole, tomate y cebolla picado)',
+                                price: '19.000'
+                                },
+                                {
+                                id : 4,    
+                                name: 'Hamburguesa Campesina',
+                                description: '(Maiz tierno, huevo duro picado y salsas)',
+                                price: '19.000'
+                                },
+                                {
+                                id : 5,    
+                                name: 'Hamburguesa Pollo',
+                                description: '(Pechuga a la plancha, cebolla caramelizada y salsas)',
+                                price: '19.500'
+                                },
+                                {
+                                id : 6,    
+                                name: 'Hamburguesa Criolla',
+                                description: '(cebolla grille, huevo frito y salsas)',
+                                price: '19.000'
+                                }
+                    ]
+                    },
+                    { title: 'HOT DOG',
+                    products:[
+                                {
+                                id : 1,    
+                                name: 'Hot Dog Ternera',
+                                description: '(Salchicha Alemana de Ternera, cebolla caramelizada, platanito maduro, queso rallado, papa en casco y salsas).',
+                                price: '17.500'
+                                },
+                                {
+                                id : 2,    
+                                name: 'Hot Dog Res',
+                                description: '(Salchicha Alemana de Res, cebolla caramelizada, platanito maduro, queso rallado, papa en casco y salsas).',
+                                price: '17.500'
+                                }
+                    ]
+                    },
+                    { title: 'SANDWICH',
                     products:[
                                 {
                                 id : 1,    
                                 name: 'Pollo',
-                                description: 'presa de pollo',
-                                price: '3.000'
+                                description: '(Pollo desmenuzado, queso, cebolla caramelizada, tomate)',
+                                price: '15.000'
                                 },
                                 {
                                 id : 2,    
-                                name: 'carne',
-                                description: 'un trosote de carte',
-                                price: '5.000'
+                                name: 'Lomo',
+                                description: '(Lomo fino, doble queso, tomate,lechuga,cebolla caramelizada)',
+                                price: '19.000'
+                                },
+                                {
+                                id : 3,    
+                                name: 'Jamón',
+                                description: '(Jamón artesanal, doble queso, tomate, cebolla caramelizada)',
+                                price: '15.500'
                                 }
                     ]
                     },
-                    { title: 'ensaladas',
+                    { title: 'ENSALADA',
                     products:[
                                 {
                                 id : 1,    
-                                name: 'ensaladas de Pollo',
-                                description: 'presa de pollo',
-                                price: '3.000'
+                                name: 'Ensalada Cork',
+                                description: 'Trozos de pechuga de pollo, crutones, maiz, lechuga, tomate, pimentón, queso y champiñones.',
+                                price: '15.000'
+                                }
+                    ]
+                    },
+                    { title: 'PICADAS',
+                    products:[
+                                {
+                                id : 1,    
+                                name: 'Picada Parrilla',
+                                description: '(Churrasco, carne de cerdo, pechuga de pollo y salchicha alemana cortada en trozos, acompañado de patatas, tomates y salsas de la casa)',
+                                price: '44.000'
                                 },
                                 {
                                 id : 2,    
-                                name: 'ensaladas de carne',
-                                description: 'un trosote de carte',
-                                price: '5.000'
+                                name: 'Picada Galway',
+                                description: '(Salchicha alemana cortada en trozos, nuggets de pollo, papas a la francesa o anillos de cebolla o croquetas de yuca)',
+                                price: '30.000'
+                                },
+                                {
+                                id : 3,    
+                                name: 'Picada Joshua',
+                                description: '(Nuggets de pollo, salchicha alemana cortada en trozos, colombinas de pollo, croquetas de yuca, anillos de cebolla o papas a la francesa, acompañada de salsas)',
+                                price: '40.000'
+                                },
+                                {
+                                id : 4,    
+                                name: 'Picada Cork',
+                                description: '(Salchicha alemana cortada en trozos, empanaditas Joshua, papas a la francesa o anillos de cebolla o croquetas de yuca)',
+                                price: '30.000'
+                                },
+                    ]
+                    },
+                    { title: 'ADICIONALES',
+                    products:[
+                                {
+                                id : 1,    
+                                name: 'Anillos de cebolla',
+                                description: '',
+                                price: '6.000'
+                                },
+                                {
+                                id : 2,    
+                                name: 'Papas a la francesa',
+                                description: '',
+                                price: '6.000'
+                                },
+                                {
+                                id : 3,    
+                                name: 'Croquetas de yuca',
+                                description: '',
+                                price: '6.000'
+                                },
+                                {
+                                id : 4,    
+                                name: 'Papa en casco',
+                                description: '',
+                                price: '6.000'
                                 }
                     ]
                     }
